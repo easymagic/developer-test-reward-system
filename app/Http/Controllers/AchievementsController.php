8f7666c5d0163da88adb4f78600fd18ff4ea1d2e
easymagic <nnamware@yahoo.com>
2021-12-16 18:03:33 +0100

    np

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,19 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\UserActivityInterface;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
-    public function index(User $user)
+    public function index(User $user,UserActivityInterface $userActivityInterface)
     {
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $userActivityInterface->getUnlockedAchievements($user->id),
+            'next_available_achievements' => [
+                         'lessons_watched'=>$userActivityInterface->getNextAvailableLessonAchievements($user->id),
+                         'comments_written'=>$userActivityInterface->getNextAvailableCommentAchievements($user->id)
+             ],
+            'current_badge' => $userActivityInterface->getCurrentBadge($user->id),
+            'next_badge' => $userActivityInterface->getNextBadgeFromCurrentBadge($user->id),
+            'remaing_to_unlock_next_badge' => $userActivityInterface->getRemainingToUnlockNextBadge($user->id)
         ]);
+
+        // return [
+        //     'unlocked_achievements'=>$userActivityInterface->getUnlockedAchievements($user),
+        //     'next_available_achievements'=>[
+        //         'lessons_watched'=>$userActivityInterface->getNextAvailableLessonAchievements($user),
+        //         'comments_written'=>$userActivityInterface->getNextAvailableCommentAchievements($user)
+        //     ],
+        //     'current_badge'=>$userActivityInterface->getCurrentBadge($user),
+        //     'next_badge'=>$userActivityInterface->getNextBadgeFromCurrentBadge($user),
+        //     'remaining_to_unlock_next_badge'=>$userActivityInterface->getRemainingToUnlockNextBadge($user)
+        //   ];
+
     }
 }
